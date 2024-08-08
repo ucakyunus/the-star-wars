5fcc7a3064bb9bb1fd3b7a1c1a32187a6ae57e6f
Yunus UÇAK <yunus.ucak@outlook.com>
2024-08-08 03:41:53 +0300

    add debounce for input search

--- components/ui/search-input/index.tsx
+++ components/ui/search-input/index.tsx
@@ -1,26 +1,33 @@
 'use client';
 
+import { memo, useCallback } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import {InputAdornment} from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from '@mui/icons-material/Search';
+
+import debounce from "@mui/utils/debounce";
 
 const SearchInput = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  const handleSearch = (term: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    
-    replace(`${pathname}?${params.toString()}`);
-  }
+  const handleSearch = useCallback(
+    debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+      const term = event.target.value;
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      
+      replace(`${pathname}?${params.toString()}`);
+    }, 500),
+    []
+  );
   
   return (
     <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
@@ -34,7 +41,7 @@ const SearchInput = () => {
           fullWidth
           placeholder="Search..."
           id="fullWidth"
-          onChange={(event)=> handleSearch(event.target.value)}
+          onChange={handleSearch}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -49,4 +56,4 @@ const SearchInput = () => {
   );
 }
 
-export default SearchInput;
+export default memo(SearchInput);
--- utils/helper.ts
+++ utils/helper.ts
@@ -39,4 +39,12 @@ export const fetchData = async <T>(url: string): Promise<T> => {
 
 export const filterFulfilled = <T>(results: PromiseSettledResult<T>[]): T[] =>
   results.filter((result): result is PromiseFulfilledResult<T> => result.status === 'fulfilled').map(result => result.value);
+
+const debounce = <F extends (...args: any[]) => void>(func: F, wait: number): F => {
+  let timeout: ReturnType<typeof setTimeout>;
+  return ((...args: Parameters<F>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  }) as F;
+};
   
